caeb1350e97ea1cdb65d6b9d8e65a41b104f452c
aitbrahim <aitbrahim.mostapha@gmail.com>
2018-06-26 07:12:06 +0100

    create deployment

--- k8s/client/api_client.py
+++ k8s/client/api_client.py
@@ -1,5 +1,6 @@
 import requests
 from k8s.config.configuration import load_config
+import yaml
 
 
 class ApiClient(object):
@@ -9,14 +10,28 @@ class ApiClient(object):
 
     def api_call(self, method, url, **kwargs):
 
+        headers = kwargs.get('headers')
+        headers.update({
+            'Authorization': self.config.token
+        })
+
         if method == 'GET':
             url_api = '{}/{}'.format(self.config.host, url)
-            r = requests.get(url_api, verify=self.config.ssl_ca_cert_path, headers={
-                'Accept': 'application/json',
-                'Authorization': self.config.token
-            })
-            r.raise_for_status()
-            return r.json()
+            response = requests.get(
+                url_api,
+                verify=self.config.ssl_ca_cert_path,
+                headers=headers
+            )
+            response.raise_for_status()
+            return response.json()
 
         if method == 'POST':
-            pass
+            url_api = '{}/{}'.format(self.config.host, url)
+            response = requests.post(
+                url_api,
+                data=kwargs.get('data'),
+                verify=self.config.ssl_ca_cert_path,
+                headers=headers
+            )
+            response.raise_for_status()
+            return response.content
--- k8s/client/core_v1_api.py
+++ k8s/client/core_v1_api.py
@@ -12,7 +12,11 @@ class CoreV1Api(object):
         if namespace:
             url = 'api/v1/namespaces/{}/pods'.format(namespace)
 
-        return self.api_client.api_call('GET', url)
+        headers = {
+            'Content-type': 'application/json'
+        }
+
+        return self.api_client.api_call('GET', url, headers=headers)
 
     def list_services(self, **kwargs):
 
@@ -20,5 +24,21 @@ class CoreV1Api(object):
         url = 'api/v1/services'
         if namespace:
             url = 'api/v1/namespaces/{}/services'.format(namespace)
+        headers = {
+            'Content-type': 'application/json'
+        }
 
-        return self.api_client.api_call('GET', url)
+        return self.api_client.api_call('GET', url, headers=headers)
+
+    def submit_deployment(self, **kwargs):
+        if ('data' not in kwargs) or (kwargs['data'] is None):
+            raise ValueError("Missing the required parameter `data` when calling `submit_deployment`")
+
+        data = kwargs.get('data')
+        namespace = kwargs.get('namespace', 'default')
+        url = 'apis/extensions/v1beta1/namespaces/{}/deployments'.format(namespace)
+        headers = {
+            'Content-type': 'application/yaml'
+        }
+        return self.api_client.api_call('POST', url, data=data, headers=headers)
+
